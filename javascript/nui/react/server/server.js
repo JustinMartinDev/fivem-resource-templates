6a7d9c32374390c399387a069b2bf13bbba0da0e
Justin Martin <justin@kezios.fr>
2024-08-15 23:36:45 +0200

    update javascript nui react version

--- javascript/nui/react/server/server.js
+++
@@ -1,8 +0,0 @@
-import { myRandomData } from "./MyOther.server";
-
-on("onResourceStart", (resName) => {
-  if (resName === GetCurrentResourceName()) {
-    console.log("TypeScript boilerplate started!");
-    console.log(myRandomData);
-  }
-});
